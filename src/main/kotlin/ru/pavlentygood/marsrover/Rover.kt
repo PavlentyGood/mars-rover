f0bd7f7a5f5b845cde4c3cbcf126c8c3cfd5798f
pavlentygood <pavlentygood@yandex.ru>
2024-05-29 00:28:57 +0300

    field size

--- src/main/kotlin/ru/pavlentygood/marsrover/Rover.kt
+++ src/main/kotlin/ru/pavlentygood/marsrover/Rover.kt
@@ -4,25 +4,31 @@ import ru.pavlentygood.marsrover.Action.*
 import ru.pavlentygood.marsrover.Direction.*
 
 class Rover(
+    val width: Int,
+    val height: Int,
     val x: Int,
     val y: Int,
     val direction: Direction,
 ) {
     fun doAction(action: Action) = when (action) {
         MOVE -> Rover(
+            width = width,
+            height = height,
             x = when (direction) {
-                EAST -> x + 1
-                WEST -> x - 1
+                EAST -> if (x < width) x + 1 else 0
+                WEST -> if (x > 0) x - 1 else 0
                 else -> x
             },
             y = when (direction) {
-                NORTH -> y + 1
-                SOUTH -> y - 1
+                NORTH -> if (y < height) y + 1 else 0
+                SOUTH -> if (y > 0) y - 1 else 0
                 else -> y
             },
             direction = direction
         )
         RIGHT -> Rover(
+            width = width,
+            height = height,
             x = x,
             y = y,
             direction = when (direction) {
@@ -33,6 +39,8 @@ class Rover(
             }
         )
         LEFT -> Rover(
+            width = width,
+            height = height,
             x = x,
             y = y,
             direction = when (direction) {
--- src/test/kotlin/ru/pavlentygood/marsrover/RoverTest.kt
+++ src/test/kotlin/ru/pavlentygood/marsrover/RoverTest.kt
@@ -10,21 +10,21 @@ class RoverTest {
     @Test
     fun `move to every directions`() {
         fun move(direction: Direction, x: Int, y: Int) {
-            val origin = Rover(x = 0, y = 0, direction = direction)
+            val origin = Rover(width = 2, height = 2, x = 1, y = 1, direction = direction)
             val result: Rover = origin.doAction(MOVE)
             result.x shouldBe x
             result.y shouldBe y
         }
-        move(NORTH, 0, 1)
-        move(EAST, 1, 0)
-        move(SOUTH, 0, -1)
-        move(WEST, -1, 0)
+        move(NORTH, 1, 2)
+        move(EAST, 2, 1)
+        move(SOUTH, 1, 0)
+        move(WEST, 0, 1)
     }
 
     @Test
     fun `rotate to right and left`() {
         fun turn(action: Action, begin: Direction, end: Direction) {
-            val origin = Rover(x = 0, y = 0, direction = begin)
+            val origin = Rover(width = 0, height = 0, x = 0, y = 0, direction = begin)
             val result: Rover = origin.doAction(action)
             result.x shouldBe 0
             result.y shouldBe 0
@@ -38,5 +38,19 @@ class RoverTest {
         turn(LEFT, WEST, SOUTH)
         turn(LEFT, SOUTH, EAST)
         turn(LEFT, EAST, NORTH)
+    }
+
+    @Test
+    fun `field limit`() {
+        fun move(direction: Direction) {
+            val origin = Rover(width = 0, height = 0, x = 0, y = 0, direction = direction)
+            val rover = origin.doAction(MOVE)
+            rover.x shouldBe 0
+            rover.y shouldBe 0
+        }
+        move(NORTH)
+        move(EAST)
+        move(SOUTH)
+        move(WEST)
     }
 }
