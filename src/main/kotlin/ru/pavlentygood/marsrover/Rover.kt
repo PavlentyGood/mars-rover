76017b085c9e5da480a814667f8db3df77e722b8
pavlentygood <pavlentygood@yandex.ru>
2024-06-03 16:18:50 +0300

    refactor Rover

--- src/main/kotlin/ru/pavlentygood/marsrover/Rover.kt
+++ src/main/kotlin/ru/pavlentygood/marsrover/Rover.kt
@@ -11,30 +11,38 @@ class Rover(
     val direction: Direction,
 ) {
     fun doAction(action: Action) = when (action) {
-        MOVE -> Rover(
-            width = width,
-            height = height,
-            x = when (direction) {
-                EAST -> if (x < width) x + 1 else x
-                WEST -> if (x > 0) x - 1 else 0
-                else -> x
-            },
-            y = when (direction) {
-                NORTH -> if (y < height) y + 1 else y
-                SOUTH -> if (y > 0) y - 1 else 0
-                else -> y
-            },
-            direction = direction
-        )
+        MOVE -> moved()
         RIGHT -> directed(direction.onRight())
         LEFT -> directed(direction.onLeft())
     }
 
-    private fun directed(direction: Direction) = Rover(
-        width = width,
-        height = height,
-        x = x,
-        y = y,
-        direction = direction
-    )
+    private fun moved() =
+        Rover(
+            width = width,
+            height = height,
+            x = x + nextOffsetX(),
+            y = y + nextOffsetY(),
+            direction = direction
+        )
+
+    private fun nextOffsetX() = when (direction) {
+        EAST -> if (x < width) 1 else 0
+        WEST -> if (x > 0) -1 else 0
+        else -> 0
+    }
+
+    private fun nextOffsetY() =  when (direction) {
+        NORTH -> if (y < height) 1 else 0
+        SOUTH -> if (y > 0) -1 else 0
+        else -> 0
+    }
+
+    private fun directed(direction: Direction) =
+        Rover(
+            width = width,
+            height = height,
+            x = x,
+            y = y,
+            direction = direction
+        )
 }
