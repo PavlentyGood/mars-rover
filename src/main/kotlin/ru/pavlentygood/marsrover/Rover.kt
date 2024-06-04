cce9f312c8ec967e39187035f5b81653e9a97c26
pavlentygood <pavlentygood@yandex.ru>
2024-06-04 17:58:11 +0300

    create Vector

--- src/main/kotlin/ru/pavlentygood/marsrover/Rover.kt
+++ src/main/kotlin/ru/pavlentygood/marsrover/Rover.kt
@@ -4,43 +4,36 @@ import ru.pavlentygood.marsrover.Action.*
 import ru.pavlentygood.marsrover.Direction.*
 
 class Rover(
-    val width: Int,
-    val height: Int,
-    val x: Int,
-    val y: Int,
+    val x: Vector,
+    val y: Vector,
     val direction: Direction,
 ) {
     fun doAction(action: Action) = when (action) {
-        MOVE -> moved()
+        MOVE -> createRover(x = offsetX(), y = offsetY())
         RIGHT -> directed(direction.onRight())
         LEFT -> directed(direction.onLeft())
     }
 
-    private fun moved() =
-        createRover(
-            x = x + nextOffsetX(),
-            y = y + nextOffsetY()
-        )
-
-    private fun nextOffsetX() = when (direction) {
-        EAST -> if (x < width) 1 else 0
-        WEST -> if (x > 0) -1 else 0
-        else -> 0
+    private fun offsetX() = when (direction) {
+        EAST -> x.next()
+        WEST -> x.prev()
+        else -> x
     }
 
-    private fun nextOffsetY() =  when (direction) {
-        NORTH -> if (y < height) 1 else 0
-        SOUTH -> if (y > 0) -1 else 0
-        else -> 0
+    private fun offsetY() = when (direction) {
+        NORTH -> y.next()
+        SOUTH -> y.prev()
+        else -> y
     }
 
     private fun directed(direction: Direction) =
         createRover(direction = direction)
 
-    private fun createRover(x: Int = this.x, y: Int = this.y, direction: Direction = this.direction) =
+    private fun createRover(
+        x: Vector = this.x,
+        y: Vector = this.y,
+        direction: Direction = this.direction) =
         Rover(
-            width = width,
-            height = height,
             x = x,
             y = y,
             direction = direction
--- src/test/kotlin/ru/pavlentygood/marsrover/RoverTest.kt
+++ src/test/kotlin/ru/pavlentygood/marsrover/RoverTest.kt
@@ -1,6 +1,7 @@
 package ru.pavlentygood.marsrover
 
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import org.junit.jupiter.api.Test
 import ru.pavlentygood.marsrover.Action.*
 import ru.pavlentygood.marsrover.Direction.*
@@ -9,44 +10,31 @@ class RoverTest {
 
     @Test
     fun `move to every directions`() {
-        fun move(direction: Direction, x: Int, y: Int) {
-            val origin = Rover(width = 2, height = 2, x = 1, y = 1, direction = direction)
-            val result: Rover = origin.doAction(MOVE)
-            result.x shouldBe x
-            result.y shouldBe y
-        }
-        move(NORTH, 1, 2)
-        move(EAST, 2, 1)
-        move(SOUTH, 1, 0)
-        move(WEST, 0, 1)
+        val x = vector(1)
+        val y = vector(1)
+
+        fun moved(direction: Direction) =
+            Rover(x = x, y = y, direction = direction)
+                .doAction(MOVE)
+
+        moved(NORTH).y shouldNotBe y
+        moved(EAST).x shouldNotBe x
+        moved(SOUTH).y shouldNotBe y
+        moved(WEST).x shouldNotBe x
     }
 
     @Test
     fun `rotate to right and left`() {
         fun turn(action: Action, begin: Direction, end: Direction) {
-            val origin = Rover(width = 0, height = 0, x = 0, y = 0, direction = begin)
+            val origin = Rover(x = vector(0), y = vector(0), direction = begin)
             val result: Rover = origin.doAction(action)
-            result.x shouldBe 0
-            result.y shouldBe 0
+            result.x shouldBe origin.x
+            result.y shouldBe origin.y
             result.direction shouldBe end
         }
         turn(action = RIGHT, begin = NORTH, end = EAST)
         turn(action = LEFT, begin = WEST, end = SOUTH)
     }
 
-    @Test
-    fun `field limit`() {
-        fun move(direction: Direction, resultX: Int, resultY: Int) {
-            val origin = Rover(width = 1, height = 1, x = 0, y = 0, direction = direction)
-            val rover = origin
-                .doAction(MOVE)
-                .doAction(MOVE)
-            rover.x shouldBe resultX
-            rover.y shouldBe resultY
-        }
-        move(NORTH, 0, 1)
-        move(EAST, 1, 0)
-        move(SOUTH, 0, 0)
-        move(WEST, 0, 0)
-    }
+    private fun vector(value: Int) = Vector(value = value, limit = 2)
 }
