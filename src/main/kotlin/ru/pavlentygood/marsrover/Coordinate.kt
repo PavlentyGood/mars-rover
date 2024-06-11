a862c9be6d363eff303178d9e791d43913802a30
pavlentygood <pavlentygood@yandex.ru>
2024-06-11 21:37:24 +0300

    add const for min limit

--- src/main/kotlin/ru/pavlentygood/marsrover/Coordinate.kt
+++ src/main/kotlin/ru/pavlentygood/marsrover/Coordinate.kt
@@ -1,12 +1,14 @@
 package ru.pavlentygood.marsrover
 
+const val MIN_LIMIT = 0
+
 data class Coordinate(
     val value: Int,
     val limit: Int
 ) {
     fun add(offset: Int) =
         (value + offset).let {
-            if (it < 0 || it > limit) this
+            if (it < MIN_LIMIT || it > limit) this
             else Coordinate(it, limit)
         }
 
