4178a12f8b1efd776f8f4c8e53d513ae3ca881b3
pavlentygood <pavlentygood@yandex.ru>
2024-06-12 18:22:54 +0300

    remove spring context running

--- src/main/kotlin/ru/pavlentygood/marsrover/MarsRoverApplication.kt
+++ src/main/kotlin/ru/pavlentygood/marsrover/MarsRoverApplication.kt
@@ -1,16 +1,11 @@
 package ru.pavlentygood.marsrover
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 import ru.pavlentygood.marsrover.console.Console
 import ru.pavlentygood.marsrover.usecase.ExploreMarsScenario
 
-@SpringBootApplication
 class MarsRoverApplication
 
-fun main(args: Array<String>) {
-    runApplication<MarsRoverApplication>(*args)
-
+fun main() {
      while (true) {
          Console(System.`in`, System.out, ExploreMarsScenario())
              .process()
